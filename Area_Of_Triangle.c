cdacd2d797f26f5dea0fa91e3770eead4b5a79d5
RautMaya <suryawanshimaya28@gmail.com>
2024-02-08 21:20:41 +0530

    Practice Program

---
+++ Area_Of_Triangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float base,height,area;
+    printf("Enter base and height of triangle");
+    scanf("%f",&height);
+    scanf("%f",&base);
+    area=base*height/2;
+    printf("area of triangle=%f",area);
+
+}
