023c860fc2d71a2185d290e80dc802248bbec9fb
RautMaya <suryawanshimaya28@gmail.com>
2024-02-08 21:37:14 +0530

    Practice Program

---
+++ ManyOperation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum,sub,multi,mod,division;
+    printf("enter two numbers");
+    sum=a+b;
+    sub=a-b;
+    multi=a*b;
+    mod=a%b;
+    division=a/b;
+    printf("sum,sub,multi,mod,division");
+    return 0;
+}
