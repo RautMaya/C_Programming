e75c0330d782c46b85386bd29b8a653cdc86e4bf
RautMaya <suryawanshimaya28@gmail.com>
2024-02-08 21:35:06 +0530

    Practice Program

---
+++ Greatter.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    int a,b;
+    printf("enter two number");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+        printf("the given number %d is gretter",a);
+    else
+        printf("the given number %d is gretter",b);
+    return 0;
+}
