e10dc6bd3b5fea25261faf0d8b52a1ff6078320f
RautMaya <suryawanshimaya28@gmail.com>
2024-02-08 21:49:11 +0530

    Practice Program

---
+++ Meter.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float kilometer,meter;
+    printf("Enter value in kilometer");
+    scanf("%f",&kilometer);
+    meter = kilometer*1000;
+    printf("value in meter is=%f",meter);
+    return 0;
+}
