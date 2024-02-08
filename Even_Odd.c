06065a20433881b42376f373c342672a09e4e41f
RautMaya <suryawanshimaya28@gmail.com>
2024-02-08 21:15:11 +0530

    practice program

---
+++ Even_Odd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    int n;
+    printf("enter any number");
+    scanf("%d",&n);
+    if(n%2==0)
+        printf("the given number %d is even",n);
+    else
+        printf("the given number %d is odd",n);
+    return 0;
+}
