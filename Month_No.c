2f6737a01cb9356d0b4f91a340074f38d226c68e
RautMaya <suryawanshimaya28@gmail.com>
2024-02-09 22:02:08 +0530

    Practice Program

---
+++ Month_No.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main()
+{
+    switch(N)
+    {
+    int N;
+    printf("Enter of days");
+    scanf("%d",&N);
+
+    case1:
+    case3:
+    case5:
+    case7:
+    case8:
+    case10:
+    case12:
+        printf("31 days");
+        break;
+    case4:
+    case6:
+    case9:
+    case11:
+    case8:
+        printf("30 days");
+        break;
+    case2:
+        printf("28|29 days");
+     default:
+         printf("Invalid Month");
+         break;
+
+    }
+    return 0;
+}
