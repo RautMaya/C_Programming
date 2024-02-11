a0218d160590ae75819692d7e7fcb3f48c63f0f9
RautMaya <suryawanshimaya28@gmail.com>
2024-02-11 21:37:33 +0530

    Practice Program

---
+++ Week_No.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int day;
+    printf("Enter any number of week");
+    scanf("%d",&day);
+   switch(day)
+   {
+       int week;
+       printf("Enter Week number(1-7)");
+       scanf("%d",&week);
+
+            case1:("Monday")
+            break;
+            case1:("Tuesday")
+            break;
+            case1:("Thursday")
+            break;
+            case1:("Friday")
+            break;
+            case1:("Saturday")
+            break;
+            case1:("Sunday")
+            break;
+            dafault:("Invalid input Enter week number (1-7)");
+   }
+   return 0;
+}
+
+
+
