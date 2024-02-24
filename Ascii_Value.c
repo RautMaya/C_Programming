3888be3d111339e109e585be2fabbd9dfa3e54c1
RautMaya <suryawanshimaya28@gmail.com>
2024-02-25 01:49:54 +0530

    Practice Program

---
+++ Ascii_Value.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    char c;
+    double d;
+    printf("ASCII value of integer is =%z",size of (a));
+    printf("ASCII value of float is =%z",size of (b));
+    printf("ASCII value of char is =%z",size of (c));
+    printf("ASCII value of double is =%z",size of (d));
+    return 0;
+
+}
