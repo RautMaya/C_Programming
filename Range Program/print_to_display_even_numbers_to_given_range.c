6ba8213deb4e5b1cda74d005de9e6de734c38ecd
RautMaya <suryawanshimaya28@gmail.com>
2024-02-27 12:35:27 +0530

    Practice Program

---
+++ Range Program/print_to_display_even_numbers_to_given_range.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int range;
+    printf("Enter any range");
+    scanf("%d",&range);
+    for(i=1;i<=range;i++)
+    {
+        if(i%2==0)
+            prinf("%d,i");
+    }
+    return 0;
+}
