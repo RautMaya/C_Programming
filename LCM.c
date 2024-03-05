f6dc458d339b21499f036838fde2175a1d06bbde
RautMaya <suryawanshimaya28@gmail.com>
2024-03-06 00:46:33 +0530

    Practice Program

---
+++ LCM.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n1,n2,gcd=1,i,LCM;
+    printf("Enter any number");
+    scanf("%d%d",&n1,&n2);
+    for(i=1;i<=n1 && i<=n2 ;i++);
+    {
+        if(n1%==0 && n2%==0)
+        gcd==i;
+    }
+    LCM=(n1*n2)/gcd;
+    printf("given LCM is %d:",LCM);
+    return 0;
+}
