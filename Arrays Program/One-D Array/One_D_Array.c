32e2244d81aafb79b5b0e0a6932844e0d9814c01
RautMaya <suryawanshimaya28@gmail.com>
2024-03-14 21:38:15 +0530

    Practice Program

---
+++ Arrays Program/One-D Array/One_D_Array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int a[5];
+    int i;
+    cout<<"Enter Array Element:";
+    for(i=0;i<=5;i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"Array Elements are:";
+    for(i=0;i<=5;i++)
+    {
+        cin>>a[i];
+    }
+    getch();
+}
+
