2b38c8b4cab220c36256d5b6ab8f161298f969c1
RautMaya <suryawanshimaya28@gmail.com>
2024-03-15 16:24:19 +0530

    Practice Program

---
+++ Pattern/Character Printing/Character_Printing_Using_While_Loop.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int i=1,j=0;
+    int rows=5;
+    char character='A';
+
+    while(i<=rows)
+    {
+        while(j<=i-1)
+        {
+            printf("%c",character);
+            j++;
+        }
+        printf("\n");
+        character++;
+        j=0;
+        i++;
+    }
+    return 0;
+}
