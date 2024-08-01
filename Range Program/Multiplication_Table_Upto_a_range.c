683a67e4e17173a8e8618d52c461fc16a3a5b2cb
RautMaya <suryawanshimaya28@gmail.com>
2024-08-01 15:20:50 +0530

    Practice Program

--- Range Program/Multiplication_Table_Upto_a_range.c
+++ Range Program/Multiplication_Table_Upto_a_range.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,range;
+    int num,i,range;
     printf("Enter an integer:");
-    scanf("%d",&n);
+    scanf("%d",&num);
 
     do{
         printf("Enter the range(Positive integer):");
@@ -13,7 +13,7 @@ int main()
 
     for(i=1;i<=range;++i)
     {
-        printf("%d*%d=%d\n",n,i,n*i);
+        printf("%d*%d=%d\n",num,i,num*i);
     }
     return 0;
 }
