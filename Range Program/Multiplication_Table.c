fdc4fab6526bed711a6497a2391826a749d162a4
RautMaya <suryawanshimaya28@gmail.com>
2024-09-20 14:59:38 +0530

    Practice Program

--- Range Program/Multiplication_Table.c
+++ Range Program/Multiplication_Table.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int n,i;
+    int num,i;
     printf("Enter an integer:");
-    scanf("%d",&n);
+    scanf("%d",&num);
 
     for(i=1;i<=8;i++)
     {
-        printf("%d*%d=%d\n",n,i,n*i);
+        printf("%d*%d=%d\n",num,i,num*i);
     }
     return 0;
 }
