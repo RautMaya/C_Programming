c628f6d00794edf6f532995d7bad25d4962f882e
RautMaya <suryawanshimaya28@gmail.com>
2024-10-20 17:39:02 +0530

    Maximum No

---
+++ To Find Maximu No/Maximum_Between_Two_No.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two number");
+    scanf("%d%d",&a,&b);
+    if(a>b)
+        prinf("The given number %d is greater",a);
+    else
+    prinf("The given number %d is greater",b);
+    return 0;
+
+}
