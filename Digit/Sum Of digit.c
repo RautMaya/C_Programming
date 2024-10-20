26957026781807e84d42a4fa7437609ec5198569
RautMaya <suryawanshimaya28@gmail.com>
2024-10-20 17:45:31 +0530

    Sum of Digits

--- Digit/Sum Of digit.c
+++ Digit/Sum Of digit.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int iNum=0,temp=0,dig=0,Cnt=0;
+    int iNum=0,temp=0,dig=0,iCnt=0;
 
     printf("Enter Number");
     scanf("%d",&iNum);
@@ -13,12 +13,12 @@ int main()
     {
         dig=temp%10;
 
-        Cnt=Cnt+dig;
+        iCnt=iCnt+dig;
 
 
         temp=temp/10;
     }
-    printf("Sum of Digit is %d =%d",iNum,Cnt);
+    printf("Sum of Digit is %d =%d",iNum,iCnt);
 
     getch();
     return 0;
