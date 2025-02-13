ee059468707fffc3d644496eca8e0567ae93f046
RautMaya <suryawanshimaya28@gmail.com>
2025-02-13 16:51:36 +0530

    Bitwise XOR Operation

---
+++ Bitwise Program/03 Write a Program to Perform the bitwise XOR Operation between two integer.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    int No1 = 0,No2 = 0, Res =0;
+
+    printf("\n Enter a 1st Number :");
+    scanf("%d",&No1);
+    printf("\n Enter a 2nd number :");
+    scanf("%d",&No2);
+
+    Res = No1 ^ No2;
+
+    printf("\n Result is => %d | %d = %d",No1,No2,Res);
+
+    getch();
+    return 0;
+}
+
