43b120749d7271e5ba2d966c61aac3309bd64183
RautMaya <suryawanshimaya28@gmail.com>
2025-02-14 19:34:30 +0530

    Right Shift Operation

---
+++ Bitwise Program/06 Write a Program to Perform the bitwise Right shift operation on an integer.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 0,Res = 0;
+
+    printf("\n Enter a Number : ");
+    scanf("%d",&No1);
+
+    Res = No1 >> 2;
+
+    printf("\n Result of %d = >> %d",No1,Res);
+
+    getch();
+    return 0;
+}
