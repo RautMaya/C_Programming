f0749f134f56d9a72f7488b94306d09efc1bbc65
RautMaya <suryawanshimaya28@gmail.com>
2025-02-28 12:02:27 +0530

    Pattern

---
+++ Pattern Printing/17 Pattern.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0,j = 0,R = 0,C = 0;
+
+    printf("\n Enter Rows : ");
+    scanf("%d",&R);
+    printf("\n Enter Columns : ");
+    scanf("%d",&C);
+
+    for(i = 1; i <= R ; i++)
+    {
+        for(j = 0 ; j <= C ; j++)
+        {
+            printf(" %d ", C - j );
+
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
