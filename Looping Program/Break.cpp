5017f29c3c2fce52e15894235062daf311e22f93
RautMaya <suryawanshimaya28@gmail.com>
2025-03-01 18:30:28 +0530

    Break Statement

---
+++ Looping Program/Break.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+int main()
+{
+    for(int i=1;i<=8;i++)
+    {
+        if(i==3)
+        {
+          break;
+        }
+        cout<<i;
+    }
+    return 0;
+
+}
