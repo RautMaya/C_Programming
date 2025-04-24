0f83d33ac9e571e9c44cf93db2a8ebdc37130381
RautMaya <suryawanshimaya28@gmail.com>
2025-04-24 23:06:29 +0530

    Copy String

---
+++ String/String_Program_Using_Own_Function/07 Copy_String.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+
+void StringCpy(char[],char[]);
+
+int main()
+{
+    char cSrc[20] = "",cDest[20] = "";
+
+
+    printf("\n Enter a String :");
+    gets(cSrc);
+
+    StringCpy(cDest,cSrc);
+
+    printf("\n Entered String : %s",cSrc);
+    printf("\n Copy String : %s",cDest);
+
+    getch();
+    return 0;
+}
+
+void StringCpy(char Dest[],char Src[])
+{
+    int i = 0;
+
+    while(Src[i] != '\0')
+    {
+        Dest[i] = Src[i];
+      i++;
+    }
+
+    return ;
+
+}
+
+
