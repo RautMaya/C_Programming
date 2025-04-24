d5e2365d915c14fb516013c198ecbcaa36d04d3c
RautMaya <suryawanshimaya28@gmail.com>
2025-04-24 23:17:37 +0530

    Toggle Given String

---
+++ String/String_Program_Using_Own_Function/13 Toggle_Given_String.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+
+void ConvertLowerCase(char[]);
+
+int main()
+{
+    char cSrc[20] = "";
+
+
+    printf("\n Enter a String :");
+    gets(cSrc);
+
+    ConvertLowerCase(cSrc);
+
+    printf("\n After Toggle a String : %s",cSrc);
+
+    getch();
+    return 0;
+}
+
+void ConvertLowerCase(char Src[])
+{
+    int i = 0;
+
+    while(Src[i] != '\0')
+    {
+        if(Src[i] >= 65 && Src[i] <= 90)
+        {
+            Src[i] = Src[i] + 32;
+        }
+        else if(Src[i] >= 97 && Src[i] <= 122)
+        {
+            Src[i] = Src[i] - 32;
+        }
+      i++;
+    }
+    return ;
+
+}
+
+
